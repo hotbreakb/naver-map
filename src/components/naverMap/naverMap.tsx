41f6ead3d55d246a1e81976503aefcc97b664dac
hotbreakb <areyhappy12@gmail.com>
2022-04-28 16:35:11 +0900

    add marker

--- src/components/naverMap/naverMap.tsx
+++ src/components/naverMap/naverMap.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { ReactComponent as Marker } from "assets/zoopi-marker.svg";
+import ZoopiMarker from "../../assets/zoopi-marker.svg";
 
 type Location = {
   latitude: number;
@@ -26,13 +28,29 @@ const NaverMap = () => {
 
   useEffect(() => {
     // userLocation으로 이동
-    new naver.maps.Map("map", {
+    const map = new naver.maps.Map("map", {
       center: new naver.maps.LatLng(
         userLocation.latitude,
         userLocation.longitude
       ),
       zoom: 14,
       zoomControl: true,
+    });
+
+    // 마커로 위치 표시
+    const marker = new naver.maps.Marker({
+      position: new naver.maps.LatLng(
+        userLocation.latitude,
+        userLocation.longitude
+      ),
+      map,
+      icon: {
+        url: ZoopiMarker,
+        // size: new naver.maps.Size(25, 34),
+        // scaledSize: new naver.maps.Size(25, 34),
+        // origin: new naver.maps.Point(0, 0),
+        // anchor: new naver.maps.Point(12, 34),
+      },
     });
   }, [userLocation]);
 
